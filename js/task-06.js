52c8cc03fec9b8617641af734825b7e5e498dd1a
charl2on381 <charl2on381@gmail.com>
2023-10-19 15:54:39 +0800

    Update

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const textInput = document.querySelector('#validation-input');
+
+const textInputBlurHandler = event => {
+  if (event.currentTarget.value.length === Number(textInput.dataset.length)) {
+    if (textInput.classList.contains('invalid')) {
+      textInput.classList.replace('invalid', 'valid');
+    } else {
+      textInput.classList.add('valid');
+    }
+  } else {
+    if (textInput.classList.contains('valid')) {
+      textInput.classList.replace('valid', 'invalid');
+    } else {
+      textInput.classList.add('invalid');
+    }
+  }
+};
+textInput.addEventListener('blur', textInputBlurHandler);
