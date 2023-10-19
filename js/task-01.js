4b5a46d3293d54ca4c889cf470f5670e2b5f5854
charl2on381 <charl2on381@gmail.com>
2023-10-19 22:29:51 +0800

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -9,3 +9,17 @@ listItems.forEach(listItem => {
   console.log(`Category: ${listItem.firstElementChild.textContent}`);
   console.log(`Elements: ${listItem.lastElementChild.childElementCount}`);
 });
+
+
+//sir marvin Code
+// const categoriesElements = document.querySelectorAll(".item");
+// console.log("Number of categories:", categoriesElements.length);
+
+// categoriesElements.forEach((el) => {
+//   const categoryName = el.firstElementChild.textContent;
+//   console.log("Category:", categoryName);
+
+//   const categoryTypesList = el.lastElementChild;
+//   const categoryTypesLength = categoryTypesList.children.length;
+//   console.log("Elements:", categoryTypesLength);
+// });
